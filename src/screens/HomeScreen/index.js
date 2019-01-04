8eec1c99e85a2f6642360613c32c87c5937d477b
rphpacheco <rphpacheco@gmail.com>
2019-01-04 17:27:46 -0200

    first commit

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { View, Text, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native'
+import assets from './assets'
+import styles from './styles'
+
+class HomeScreen extends Component {
+    state = {
+        counter : 0
+    }
+    handlerCounter = () => {
+        this.setState({
+            counter : this.state.counter + 1
+        })
+    }
+    render() {
+        return (
+            <ImageBackground
+                source={ assets.background }
+                imageStyle={{ resizeMode : 'stretch' }}
+                style={ styles.background }
+            >
+                <View style={ styles.wrapperLogoTripPlanner }>
+                    <Image source={ assets.tripplanner } />
+                </View>
+                <View style={ styles.wrapperLogoDevPleno }>
+                    <Image source={ assets.devpleno } />
+                </View>
+                <TouchableWithoutFeedback onPress={ this.handlerCounter }>
+                    <View style={ styles.buttonBackground }>
+                        <Text style={ styles.buttonText }>
+                        COMEÇAR! { this.state.counter }</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+            </ImageBackground>
+        )
+    }
+}
+
+export default HomeScreen
